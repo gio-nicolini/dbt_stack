30200ffc30addd56c9140808d0a66fbae4d824e1
Nicolini <nicolini.dev@gmail.com>
2024-09-17 22:52:34 -0300

    feat: add first model

--- pg_stagings/models/transform_customers.sql
+++ pg_stagings/models/transform_customers.sql
@@ -2,3 +2,19 @@ with source_data as(
     SELECT *
     FROM {{ source('data_raw', 'customers_raw') }}
 )
+
+with replace_name as (
+
+    SELECT customer_id,
+            name as full_name,
+            email,
+            address,
+            city,
+            country,
+            phone_number,
+            birthdate
+    FROM source_data
+)
+
+SELECT *
+FROM replace_name
